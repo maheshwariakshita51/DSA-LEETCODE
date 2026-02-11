4dacd3f2ade1573cfdc9db80cf54bd7bc439aced
Akshita Maheshwari <maheshwariakshita6@gmail.com>
2026-02-11 11:33:09 +0530

    Time: 23 ms (98.79%) | Memory: 98.3 MB (86.76%) - LeetSync

--- 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.cpp
+++ 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.cpp
@@ -3,7 +3,7 @@ public:
     long long minimumTime(vector<int>& time, int totalTrips) {
         long long left=1;
         long long right =(long long) *min_element(time.begin(),time.end())*totalTrips;
-        while(left<=right)
+        while(left<right)
         {
         long long Trip=0;
         long long mid= left+(right-left)/2;
@@ -12,7 +12,7 @@ public:
             Trip+=mid/bus;
         }
         // if(Trip == totalTrips)  return mid;
-        if(Trip >=totalTrips) right=mid-1;
+        if(Trip >=totalTrips) right=mid;
         else left=mid+1;
         }
         return left;
