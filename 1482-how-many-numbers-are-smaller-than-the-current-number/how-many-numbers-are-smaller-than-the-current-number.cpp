47e1cbeba70851110111b18b91e3c1331bbaf1c9
Akshita Maheshwari <maheshwariakshita6@gmail.com>
2026-02-19 23:52:32 +0530

    Time: 11 ms (31.05%) | Memory: 14.2 MB (79.44%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -6,7 +6,7 @@ public:
         for(int i=0;i<n ; i++){
             for(int j=0 ;j<n ;j++)
             {
-                if(nums[j]<nums[i])
+                if(nums[i]>nums[j])
                 {
                     res[i]++;
                 }
